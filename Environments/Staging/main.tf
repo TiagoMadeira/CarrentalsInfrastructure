dd2c5593a99f135ca9c6cb4cf667252b5b040316
TiagoMadeira <tiagopmadeira@gmail.com>
2025-11-17 17:51:30 +0000

    Feat: Add APIgateway and RDS to project infranstructure

--- Environments/Staging/main.tf
+++ Environments/Staging/main.tf
@@ -28,7 +28,7 @@ module "networking"{
     environment      = "staging"
     region           = "eu-west-3"
     zone1            = "eu-west-3a"
-    zone2            = "eu-west-3b"
+    zone2            = "eu-west-3c"
 
 }
 
@@ -40,8 +40,35 @@ module "eks_cluster" {
     environment               = "staging"
     eks_name                  = "carrental-cluster"
     eks_version               = "1.33"
+    vpc_id                    = module.networking.vpc_id
     subnet_private_zone1_id   = module.networking.subnet_private_zone1_id
     subnet_private_zone2_id   = module.networking.subnet_private_zone2_id
 
     depends_on = [ module.networking ]
+}
+
+
+module "rds_db" {
+   source = "./../../Modules/RDS"
+
+   db_name                    = "StagingCarrentalDb"
+   engine                     = "postgres"
+   vpc_id                     = module.networking.vpc_id
+   subnet_private_zone1_id    = module.networking.subnet_private_zone1_id
+   subnet_private_zone2_id    = module.networking.subnet_private_zone2_id
+
+   depends_on = [ module.networking ]
+}
+
+
+module "api_gateway" {
+  source = "./../../Modules/APIgateway"
+
+  vpc_id                    = module.networking.vpc_id
+  subnet_private_zone1_id    = module.networking.subnet_private_zone1_id
+  subnet_private_zone2_id    = module.networking.subnet_private_zone2_id
+}
+
+output "signup_url" {
+  value = module.api_gateway.signup_url
 }
