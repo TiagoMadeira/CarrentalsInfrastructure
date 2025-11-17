b3b3f528f41f113807a08a3c9bd9d5507aab156b
TiagoMadeira <tiagopmadeira@gmail.com>
2025-11-17 18:02:16 +0000

    Fix: Fixed minor identation issue

--- Environments/Staging/main.tf
+++ Environments/Staging/main.tf
@@ -64,9 +64,9 @@ module "rds_db" {
 module "api_gateway" {
   source = "./../../Modules/APIgateway"
 
-  vpc_id                    = module.networking.vpc_id
-  subnet_private_zone1_id    = module.networking.subnet_private_zone1_id
-  subnet_private_zone2_id    = module.networking.subnet_private_zone2_id
+  vpc_id                      = module.networking.vpc_id
+  subnet_private_zone1_id     = module.networking.subnet_private_zone1_id
+  subnet_private_zone2_id     = module.networking.subnet_private_zone2_id
 }
 
 output "signup_url" {
